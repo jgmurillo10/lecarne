7c92898c28d8a84a15484fbfce40e37b72f9b982
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-07-26 22:09:38 -0500

    add fields

--- src/Fields.jsx
+++ src/Fields.jsx
@@ -9,6 +9,16 @@ class Fields extends Component{
           <h2>
             Por Favor selecciona los campos que deseas agregar a tu carné
           </h2>
+          <ul>
+            <li>Nombres</li>
+            <li>Apellidos</li>
+            <li>CC</li>
+            <li>Foto</li>
+            <li>Fecha de vencimiento</li>
+            <li>Logo Empresa</li>
+            <li>Nombre Empresa</li>
+
+          </ul>
         </div>
       </div>
     )
